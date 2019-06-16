fab613ae5c11c859053f81d0c7ef7685a0b1cbae
AliAdam <eng.aliadam@gmail.com>
2019-06-16 20:13:44 +0300

    Initial Commit

---
+++ NewsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsDemo
+//
+//  Created by Ali Adam on 6/16/19.
+//  Copyright © 2019 AliAdam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
